c79348ec23b3615869e831817ded8e1664ab8662
Kristin Gumbert <kristingumbert@Kristins-MacBook-Pro.local>
2017-10-05 13:49:38 -0400

    handlebars

--- server.js
+++ server.js
@@ -1,29 +1,67 @@
-
-//Dependencies
+// Dependencies
+// =============================================================
 var express = require('express');
 var bodyParser = require('body-parser');
+var nodemailer = require("nodemailer");
 var path = require('path');
+var exphbs = require('express-handlebars');
 
-//Server config properties
+// Sets up the Express App
+// =============================================================
 var app = express();
-var PORT = process.env.PORT || 7000;
+var PORT = process.env.PORT || 3000;
 
+// Sets up the Express app to handle data parsing 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(express.static(process.cwd() + '/public'));
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'index'));
+// })
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.render('index');
+});
+app.get('/about', function(req, res) {
+    res.render('about');
 })
-app.get('/cv', function(req, res) {
-    res.sendFile(path.join(__dirname, 'cv.html'));
-})
-// app.get('/resume', function(req, res) {
-//     res.sendFile(path.join(__dirname, ''));
-// })
 
-app.listen(PORT, function(){
-  console.log("App listening on port: " + PORT);
+app.get('/contact', function(req, res) {
+    res.render('contact');
+});
+
+app.get('/privacy', function(req, res) {
+    res.render('privacy');
+})
+
+app.get('/services', function(req, res) {
+    res.render('services');
+})
+
+app.get('/gallery', function(req, res) {
+    res.render('gallery');
+})
+
+
+
+// Starts the server to begin listening 
+// =============================================================
+app.listen(PORT, function() {
+    console.log('find me at port: ' + PORT);
 })
